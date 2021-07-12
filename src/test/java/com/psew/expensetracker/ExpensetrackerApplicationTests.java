108fa44439c69e820a4758d495ba1683a8821ff1
Amanuel2000 <amanuelf44@gmail.com>
2021-07-12 15:09:40 +0300

    Initial commit -Spring Boot project crated with dependencies

---
+++ src/test/java/com/psew/expensetracker/ExpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psew.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
